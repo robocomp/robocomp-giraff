9cbab97a09e6f689ddc6771562c0134c55f34d3e
Pablo Bustos <pbustos@unex.es>
2021-09-24 13:13:53 +0200

    aded etc y pyrep

---
+++ pyrep/giraff/src/CMakeFiles/ICES__home_robo02_robocomp_components_robocomp-giraff_pyrep_giraff_src.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ICES__home_robo02_robocomp_components_robocomp-giraff_pyrep_giraff_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
