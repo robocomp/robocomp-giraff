4e45b1cf28325af4061ec9eb5f62ea8424378146
Pablo Bustos <pbustos@unex.es>
2021-09-27 17:59:00 +0200

    borrado morralaa

--- agentes/mission_controller_giraff/src/CMakeFiles/mission_controller_giraff_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/mission_controller_giraff_autogen"
-  "mission_controller_giraff_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/mission_controller_giraff_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
