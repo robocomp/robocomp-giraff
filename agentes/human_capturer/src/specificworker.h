f8990387c98259d5f42ff2a1a7534987cfada77b
Ana <anacristinabejarano@gmail.com>
2021-12-13 14:53:06 +0100

    update agent human_capturer with dsr functions

--- agentes/human_capturer/src/specificworker.h
+++ agentes/human_capturer/src/specificworker.h
@@ -40,53 +40,64 @@ public:
 	SpecificWorker(TuplePrx tprx, bool startup_check);
 	~SpecificWorker();
 	bool setParams(RoboCompCommonBehavior::ParameterList params);
+    struct PersonData
+    {
+        int id;
+        RoboCompHumanCameraBody::TJoints joints;
+        // TImage roi;
+        vector<float> personCoords;
+        float orientation;
+    };
 
-    //void CameraRGBDSimplePub_pushRGBD(RoboCompCameraRGBDSimple::TImage im, RoboCompCameraRGBDSimple::TDepth dep);
-    //void HumanCameraBodyPub_newPeopleData(RoboCompHumanCameraBodyPub::PeopleData people);
 
 public slots:
 	void compute();
 	int startup_check();
 	void initialize(int period);
+
 private:
-	// DSR graph
-	std::shared_ptr<DSR::DSRGraph> G;
+    // DSR graph
+    std::shared_ptr<DSR::DSRGraph> G;
 
-	//DSR params
-	std::string agent_name;
-	int agent_id;
+    //DSR params
+    std::string agent_name;
+    int agent_id;
 
-	bool tree_view;
-	bool graph_view;
-	bool qscene_2d_view;
-	bool osg_3d_view;
+    bool tree_view;
+    bool graph_view;
+    bool qscene_2d_view;
+    bool osg_3d_view;
 
-	// DSR graph viewer
-	std::unique_ptr<DSR::DSRViewer> graph_viewer;
-	QHBoxLayout mainLayout;
-	void modify_node_slot(std::uint64_t, const std::string &type){};
-	void modify_attrs_slot(std::uint64_t id, const std::vector<std::string>& att_names){};
-	void modify_edge_slot(std::uint64_t from, std::uint64_t to,  const std::string &type){};
+    // DSR graph viewer
+    std::unique_ptr<DSR::DSRViewer> graph_viewer;
+    QHBoxLayout mainLayout;
+    void modify_node_slot(std::uint64_t, const std::string &type){};
+    void modify_attrs_slot(std::uint64_t id, const std::vector<std::string>& att_names){};
+    void modify_edge_slot(std::uint64_t from, std::uint64_t to,  const std::string &type){};
 
-	void del_edge_slot(std::uint64_t from, std::uint64_t to, const std::string &edge_tag){};
-	void del_node_slot(std::uint64_t from){};     
-	bool startup_check_flag;
+    void del_edge_slot(std::uint64_t from, std::uint64_t to, const std::string &edge_tag){};
+    void del_node_slot(std::uint64_t from){};
+    bool startup_check_flag;
+    std::shared_ptr<DSR::RT_API> rt_api;
 
-    // Variables
 
-    int max_lambda_value = 25;
-    int min_lambda_value = -25;
+
 
     // Functions
 
     RoboCompHumanCameraBody::PeopleData test_person();
-    int increase_lambda_cont(std::int64_t lambda_cont);
-    int decrease_lambda_cont(std::int64_t lambda_cont);
-    cv::Point3d dictionary_values_to_3d_point(auto item);
-    cv::Point3d cross_product(cv::Point3d p1, cv::Point3d p2);
+    std::int32_t increase_lambda_cont(std::int32_t lambda_cont);
+    std::int32_t decrease_lambda_cont(std::int32_t lambda_cont);
+    cv::Point3f dictionary_values_to_3d_point(auto item);
+    cv::Point3f cross_product(cv::Point3f p1, cv::Point3f p2);
     float get_degrees_between_vectors(cv::Point vector_1, cv::Point vector_2, std::string format);
     float calculate_orientation(RoboCompHumanCameraBody::Person person);
-    double distance_3d(cv::Point3d p1, cv::Point3d p2);
+    float distance_3d(cv::Point3f p1, cv::Point3f p2);
+    void remove_person(DSR::Node person_node, bool direct_remove); // direct_remove = false in python
+    void update_person(DSR::Node node, std::vector<float> coords, float orientation);
+    void insert_mind(std::uint64_t parent_id, std::int32_t person_id);
+    void insert_person(std::vector<float> coords, float orientation, bool direct_insert);
+    void update_graph(vector<SpecificWorker::PersonData> people_list);
     QVector3D get_person_coords(RoboCompHumanCameraBody::Person p);
 };
 
