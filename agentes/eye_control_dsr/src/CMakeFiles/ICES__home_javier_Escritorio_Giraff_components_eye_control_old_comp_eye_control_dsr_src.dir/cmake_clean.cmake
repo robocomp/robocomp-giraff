4e904ac86c36519ebe0da17ce8d6ff2627e01f06
JavierKraken <javiduran.scout@gmail.com>
2022-02-18 12:13:21 +0100

    added eye_control_dsr

---
+++ agentes/eye_control_dsr/src/CMakeFiles/ICES__home_javier_Escritorio_Giraff_components_eye_control_old_comp_eye_control_dsr_src.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ICES__home_javier_Escritorio_Giraff_components_eye_control_old_comp_eye_control_dsr_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
