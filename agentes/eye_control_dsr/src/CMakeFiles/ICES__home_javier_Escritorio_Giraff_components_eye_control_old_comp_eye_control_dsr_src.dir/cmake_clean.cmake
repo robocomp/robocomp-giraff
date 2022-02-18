959c29d4324df683ab6be38233ec76e006bb011d
JavierKraken <javiduran.scout@gmail.com>
2022-02-18 12:40:52 +0100

    deleted eye_control_dsr

--- agentes/eye_control_dsr/src/CMakeFiles/ICES__home_javier_Escritorio_Giraff_components_eye_control_old_comp_eye_control_dsr_src.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ICES__home_javier_Escritorio_Giraff_components_eye_control_old_comp_eye_control_dsr_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
